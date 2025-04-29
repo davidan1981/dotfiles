6ad9ad6c15127cd4ef5b23acc0e041013f0db9f2
David An <davidan1981@gmail.com>
2025-04-28 21:08:29 -0500

    Fix lua file

--- .config/nvim/lua/custom/plugins/davidan.lua
+++ .config/nvim/lua/custom/plugins/davidan.lua
@@ -5,24 +5,25 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- my own key mapping
-vim.keymap.set("n", ",f", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", ",q", ":q!<CR>")
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("i", "jwj", "<Esc>:w<CR>")
-vim.keymap.set("n", "tl", ":tabn<CR>")
-vim.keymap.set("n", "th", ":tabp<CR>")
+vim.keymap.set('n', ',f', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', ',q', ':q!<CR>')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'jwj', '<Esc>:w<CR>')
+vim.keymap.set('n', 'tl', ':tabn<CR>')
+vim.keymap.set('n', 'th', ':tabp<CR>')
 
--- plugins --
-local Plug = vim.fn["plug#"]
-
-vim.call("plug#begin")
-
-Plug("nvim-tree/nvim-tree.lua")
-
-vim.call("plug#end")
-
-return {}
+return {
+  -- nvim-tree
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+}
