ff0a85d023910290fb9675b5e80cbf6452cc0139
David An <davidan1981@gmail.com>
2025-06-29 13:58:59 -0500

    [nvim] close tab when a buffer quits but nvim-tree is open

--- .config/nvim/lua/custom/plugins/davidan.lua
+++ .config/nvim/lua/custom/plugins/davidan.lua
@@ -6,29 +6,50 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- my own key mapping
-vim.keymap.set('n', ',f', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', ',q', ':q!<CR>')
-vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('i', 'jwj', '<Esc>:w<CR>')
-vim.keymap.set('n', 'tl', ':tabn<CR>')
-vim.keymap.set('n', 'th', ':tabp<CR>')
-vim.keymap.set('n', ',g', '<C-w><C-w>', { desc = 'Move focus to the next available window' })
-vim.keymap.set('n', 'zh', ':tabprevious<CR>', { desc = 'Show the previous tab' })
-vim.keymap.set('n', 'zl', ':tabnext<CR>', { desc = 'Show the next tab' })
-vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Get out of edit mode' })
-vim.keymap.set('i', 'jwj', '<Esc>:w<CR>', { desc = 'Quick save' })
+vim.keymap.set("n", ",f", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", ",q", ":q!<CR>")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "jwj", "<Esc>:w<CR>")
+vim.keymap.set("n", "tl", ":tabn<CR>")
+vim.keymap.set("n", "th", ":tabp<CR>")
+vim.keymap.set("n", ",g", "<C-w><C-w>", { desc = "Move focus to the next available window" })
+vim.keymap.set("n", "zh", ":tabprevious<CR>", { desc = "Show the previous tab" })
+vim.keymap.set("n", "zl", ":tabnext<CR>", { desc = "Show the next tab" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Get out of edit mode" })
+vim.keymap.set("i", "jwj", "<Esc>:w<CR>", { desc = "Quick save" })
+
+-- This will allow nvim to close the tab when you close a buffer but nvim-tree is open
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	callback = function()
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if
+			layout[1] == "leaf"
+			and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+			and layout[3] == nil
+		then
+			vim.cmd("quit")
+		end
+	end,
+})
 
 return {
-  -- nvim-tree
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require('nvim-tree').setup {}
-    end,
-  },
+	-- nvim-tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({
+				actions = {
+					open_file = {
+						quit_on_open = false,
+					},
+				},
+			})
+		end,
+	},
 }
